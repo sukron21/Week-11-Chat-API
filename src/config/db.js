8fae8c11c1b14bf4f07f907ccd01c0568f54abe4
lol <Rahmatfurqon0@gmail.com>
2022-12-13 03:11:18 +0700

    perubahan port

--- src/config/db.js
+++ src/config/db.js
@@ -2,13 +2,13 @@
 
 // deklare library
 const pg = require('pg')
-const{DB_HOSTNAME,DB_USERNAME,DB_PASSWORD,DB_NAME,DB_PORT}=require('../helper/env')
+const{DB_HOSTNAME,DB_USERNAME,DB_PASSWORD,DB_NAME,PORT}=require('../helper/env')
 const db = new pg.Pool({
   host: DB_HOSTNAME,
   user: DB_USERNAME,
   password: DB_PASSWORD,
   database: DB_NAME,
-  port: DB_PORT
+  port: PORT
 })
 
 // cek koneksi
